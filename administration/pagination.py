b98ad6d9a6d7b2a5ca70f78af934af0e1bfce0aa
Ostap Ohorodnyk <ostap.ohorodnyk@gmail.com>
2023-11-16 11:41:34 +0200

    created the project structure

---
+++ administration/pagination.py
@@ -0,0 +1,18 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class ListPagination(PageNumberPagination):
+    page_size = 3
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "total_items": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "current": self.page.number,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "results": data,
+            }
+        )
