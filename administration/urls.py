00f3bbaf114eda7d73f1ccd8b66d72b29cd96b33
romanmyko <romanklapek@gmail.com>
2023-12-02 22:48:46 +0200

    add Approve Company

--- administration/urls.py
+++ administration/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('administrator/', views.logon_admin, name='admin_panel'),
     path('administrator/info', login_required(views.info), name = 'info'),
     path('administrator/logout', auth_views.LogoutView.as_view(next_page='admin_panel'), name='logout'),
+    path('administrator/approve_company', login_required(views.approve_company), name = 'approve_company'),
+    path('administrator/search', login_required(views.search), name = 'search'),
 ]
--- administration/views.py
+++ administration/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.hashers import check_password
 from django.shortcuts import render, redirect
 from .forms import LoginForm
 from django.contrib.auth.decorators import login_required
-from authentication.models import CustomUser
-from profiles.models import Profile
+from authentication.models import *
+from profiles.models import *
 
 
 
@@ -41,3 +41,36 @@ def info(request):
         'companys' : companys,
     }
     return render(request,'admin_info.html', content)
+
+@login_required
+def approve_company(request):
+    saved_companies = SavedCompany.objects.all()
+
+    user_info_list = []
+    not_approved_info_list = []
+
+    for company in saved_companies:
+        user_info = {
+            'email': company.user.email,
+            'name': company.user.name,
+            'edrpou': company.company.edrpou,
+            'profile_name': company.company.name,
+            'is_active ': company.company.is_registered,
+        }
+
+        if not company.company.is_registered:
+            not_approved_info_list.append(user_info)
+        elif company.company.is_registered:
+            user_info_list.append(user_info)    
+
+    content = {
+        'user_info_list': user_info_list,
+        'not_approved_info_list': not_approved_info_list,
+    }
+    return render(request, 'admin_approve_company.html', content)
+
+@login_required
+def search(request):
+    content = {
+    }
+    return render(request,'admin_settings.html', content)
