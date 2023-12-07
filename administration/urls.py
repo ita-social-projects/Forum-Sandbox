54e795a5af9446e4b6a068d904b97a980b1793db
romanmyko <romanklapek@gmail.com>
2023-12-07 20:39:35 +0200

    fin search V2

--- administration/urls.py
+++ administration/urls.py
@@ -13,7 +13,9 @@ urlpatterns = [
     path('administrator/logout/', auth_views.LogoutView.as_view(next_page='admin_panel'), name='logout'),
     path('administrator/approve_company/', login_required(views.approve_company), name='approve_company'),
     path('administrator/approve_company/erdpou/<int:id>/', login_required(views.erdpou_aproved), name='erdpou_aproved'),
-    path('administrator/approve_company/unregistered/<int:id>/', login_required(views.company_unregistered), name='company_unregistered'),
+    path('administrator/approve_company/unregistered/<int:id>/', login_required(
+        views.company_unregistered), name='company_unregistered'
+        ),
     path('administrator/company/<int:id>/', login_required(views.admin_full_company_info), name='admin_full_company_info'),
 ]
 
