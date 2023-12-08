d57ea61918ac65fa2676b8d78c9d00c19e34d5e1
Volodymyr Dukhanin <delta.dukhanin@gmail.com>
2023-12-08 04:55:31 +0100

    tests added and minor changes to views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.views import PasswordResetConfirmView
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView, RetrieveAPIView, ListAPIView, get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from .models import CustomUser
@@ -23,7 +25,8 @@ class UsersDetailView(RetrieveAPIView):
 
     def get_object(self):
         user_id = self.kwargs['id']
-        return get_object_or_404(CustomUser, id=user_id)
+        query = CustomUser.objects.filter(id=user_id, is_staff="False")
+        return get_object_or_404(query)
 
 
 class AllUsersDetailView(ListAPIView):
@@ -32,3 +35,11 @@ class AllUsersDetailView(ListAPIView):
 
     def get_queryset(self):
         return CustomUser.objects.filter(is_staff='False').order_by('id')
+
+
+
+# class CustomPasswordReset(PasswordResetConfirmView):
+#     serializer_class = CustomUserSerializer
+#     permission_class = [AllowAny]
+
+
